9839c404337d946e8b442b610ebf123ebf2dacc3
Atomnory <easports2011@live.ru>
2020-12-21 13:41:39 +0300

    Add Customer model, modify Appointment and Doctor

--- main/appoint/admin.py
+++ main/appoint/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Doctor
 from .models import Appointment
+from .models import Customer
 
 admin.site.register(Doctor)
 admin.site.register(Appointment)
+admin.site.register(Customer)
--- main/appoint/models.py
+++ main/appoint/models.py
@@ -4,9 +4,26 @@ from django.db import models
 class Doctor(models.Model):
     first_name = models.CharField('Doctor first name', max_length=50)
     last_name = models.CharField('Doctor last name', max_length=50)
+    specialization = models.CharField('Specialization', max_length=50, null=True)
 
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
+
+    def get_absolute_url(self):
+        # return f'/{self.id}'
+        return "/%i" % self.id
+
+
+class Customer(models.Model):
+    first_name = models.CharField('Customer first name', max_length=50)
+    last_name = models.CharField('Customer last name', max_length=50)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+    def get_absolute_url(self):
+        return f'/profile/{self.id}'
+        # return "/profile/%i" % self.id
 
 
 class Appointment(models.Model):
@@ -15,14 +32,20 @@ class Appointment(models.Model):
     # pause_time
 
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    customer = models.CharField('Customer name', max_length=250, blank=True)
-    status = models.BooleanField('Status')
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def check_appointment_status(self):
+        """:return: True if this appointment may be reserved. False if appointment is already reserved."""
+        if self.customer is None:
+            return True
+        else:
+            return False
 
     def __str__(self):
         return str(self.start_time)
 
     def get_absolute_url(self):
-        pass    # TODO: make f url
+        return f'/{self.doctor.id}/appoint/{self.id}'
 
     class Meta:
         ordering = ['start_time']
